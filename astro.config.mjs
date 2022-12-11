60c8129e9bb443b7c767de69c14313a3cee49221
xTalAlex <talale93@hotmail.it>
2022-12-11 02:04:42 +0100

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,17 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://lombardaascensori.it',
+  integrations: [tailwind({
+    config: {
+      applyBaseStyles: false
+    }
+  }), sitemap()]
+});
---
+++ tailwind.config.cjs
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		fontFamily: {
+			"serif": ['PT Sans'],
+			"sans": ["Nunito Sans"]
+		},
+		extend: {},
+	},
+	plugins: [
+		require('@tailwindcss/typography'),
+		require("daisyui")
+	],
+	daisyui: {
+		logs: false,
+		themes: [
+			{
+				corporate: {
+					...require("daisyui/src/colors/themes")["[data-theme=corporate]"],
+					primary: "#388DD8",
+					accent: "#FF4136",
+				}
+			},
+		]
+	},
+}
