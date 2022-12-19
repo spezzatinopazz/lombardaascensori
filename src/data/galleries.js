23fd5288b58a8f2e195d5fe0fb5fe4600af4c11a
xTalAlex <talale93@hotmail.it>
2022-12-19 02:09:08 +0100

    multiple gallery

---
+++ src/data/galleries.js
@@ -0,0 +1,76 @@
+const galleries = [];
+
+galleries[0] = {
+    title: "Ascensore 1",
+    description: null,
+    category: "Ascensori",
+    folder: "/images/gallery/ascensore1/",
+    images: [
+        'asc1_1.jpg',
+        'asc1_2.jpg',
+        'asc1_3.jpg',
+        'asc1_4.jpg',
+        'asc1_5.jpg',
+    ],
+};
+
+galleries[1] = {
+    title: "Ascensore 2",
+    description: null,
+    category: "Ascensori",
+    folder: "/images/gallery/ascensore2/",
+    images: [
+        'asc2_1.jpg',
+        'asc2_2.jpg',
+        'asc2_3.jpg',
+        'asc2_4.jpg',
+        'asc2_5.jpg',
+        'asc2_6.jpg',
+        'asc2_7.jpg',
+    ],
+};
+
+galleries[2] = {
+    title: "Ascensore 3",
+    description: null,
+    category: "Ascensori",
+    folder: "/images/gallery/ascensore3/",
+    images: [
+        'asc3_1.jpg',
+        'asc3_2.jpg',
+        'asc3_3.jpg',
+        'asc3_4.jpg',
+        'asc3_5.jpg',
+        'asc3_6.jpg',
+    ],
+};
+
+galleries[3] = {
+    title: "Elevatore 1",
+    description: null,
+    category: "Elevatori",
+    folder: "/images/gallery/elevatore1/",
+    images: [
+        'ele1_1.jpg',
+        'ele1_2.jpg',
+        'ele1_3.jpg',
+        'ele1_4.jpg',
+        'ele1_5.jpg',
+    ],
+};
+
+galleries[4] = {
+    title: "Elevatore 2",
+    description: null,
+    category: "Elevatori",
+    folder: "/images/gallery/elevatore2/",
+    images: [
+        'ele2_1.jpg',
+        'ele2_2.jpg',
+        'ele2_3.jpg',
+        'ele2_4.jpg',
+        'ele2_5.jpg',
+    ],
+};
+
+export { galleries };
